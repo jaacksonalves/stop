c562535b6f4d3802b5fb161b9cac2ff9867d68a5
Jackson <jacksonalves.pro@gmail.com>
2022-03-21 19:39:09 -0300

    Initial Commit

---
+++ src/main/java/br/com/jackson/stop/StopApplication.java
@@ -0,0 +1,13 @@
+package br.com.jackson.stop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/jackson/stop/StopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jackson.stop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
