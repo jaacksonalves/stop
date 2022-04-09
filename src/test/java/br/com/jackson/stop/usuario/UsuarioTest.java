ba48e6ba129f0f682c809c95146d07513f9a0e4a
Jackson <jacksonalves.pro@gmail.com>
2022-04-08 22:06:18 -0300

    feat(entrar-na-sala) testes feitos

---
+++ src/test/java/br/com/jackson/stop/usuario/UsuarioTest.java
@@ -0,0 +1,31 @@
+package br.com.jackson.stop.usuario;
+
+import br.com.jackson.stop.sala.SalaFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class UsuarioTest {
+
+  @Nested
+  class PodeJogarTest {
+
+    @Test
+    @DisplayName("Pode jogar quando não está jogando atualmente")
+    void teste1() {
+      Usuario usuario = new Usuario("nome");
+      assertTrue(usuario.podeJogar());
+    }
+
+    @Test
+    @DisplayName("Não pode jogar quando está jogando atualmente")
+    void teste2() {
+      Usuario usuario = new Usuario("nome");
+      usuario.adicionaSala(SalaFactory.criaSalaSemSenha());
+      assertFalse(usuario.podeJogar());
+    }
+  }
+}
